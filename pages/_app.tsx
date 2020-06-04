7648bc89ef3bcdcd522fa8ff838decea0c8110e1
Touko Hallasmaa <touko.hallasmaa@upcloud.com>
2020-06-05 00:29:09 +0300

    no need to use provider for redux

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { NextComponentType } from "next";
 import NextApp, { AppContext, AppProps } from "next/app";
-import { Provider } from "react-redux";
 import { CacheProvider } from "@emotion/core";
 import { cache } from "emotion";
 import {
@@ -16,6 +15,7 @@ import { Context } from "../server";
 
 interface Props extends AppProps, IntlConfig {
   store: ReturnType<typeof getStore>;
+  pageProps: Record<string, unknown>;
 }
 
 const intlCache = createIntlCache();
@@ -24,7 +24,7 @@ const App: NextComponentType<
   AppContext & { ctx: Context },
   Record<string, unknown>,
   Props
-> = ({ Component, store, locale, messages, router, pageProps }: Props) => {
+> = ({ Component, locale, messages, router, pageProps }: Props) => {
   const intl = createIntl(
     {
       locale,
@@ -36,13 +36,7 @@ const App: NextComponentType<
   return (
     <CacheProvider value={cache}>
       <RawIntlProvider value={intl}>
-        <Provider store={store}>
-          <NextApp
-            Component={Component}
-            pageProps={pageProps}
-            router={router}
-          />
-        </Provider>
+        <NextApp Component={Component} pageProps={pageProps} router={router} />
       </RawIntlProvider>
     </CacheProvider>
   );
