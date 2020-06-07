91a7e514e5533c9e894e1fddc2f5388956b63991
Touko Hallasmaa <touko.hallasmaa@upcloud.com>
2020-06-07 10:52:44 +0300

    add authentication helpers

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -1,3 +1,5 @@
+import callApi from "../util/api";
+
 export interface Account {
   id: number;
   firstName: string;
@@ -6,3 +8,43 @@ export interface Account {
   locale: string;
   previouslyReviewedVersion?: string;
 }
+
+export const authenticate = async (credentials: {
+  email: string;
+  password: string;
+}) =>
+  callApi("/auth", {
+    body: credentials,
+    method: "POST",
+  }).then(() => {
+    if (typeof window !== "undefined") {
+      window.location.href = "/";
+    }
+    return credentials;
+  });
+
+export const register = async (
+  account: Omit<Account, "id"> & {
+    retypePassword: string;
+    password: string;
+  },
+  authenticateAfter = true
+) =>
+  callApi("/accounts", {
+    body: account,
+    method: "POST",
+  }).then(() => {
+    if (authenticateAfter) {
+      return authenticate({ email: account.email, password: account.password });
+    }
+    return account;
+  });
+
+export const signOut = async () => {
+  await callApi("/auth", {
+    method: "DELETE",
+  });
+  if (typeof window !== "undefined") {
+    window.location.href = "/";
+  }
+};
