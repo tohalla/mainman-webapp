f2077bb12c13c3290f6d2ea4bf73090534198a7f
Touko Hallasmaa <touko.hallasmaa@upcloud.com>
2020-06-07 11:18:41 +0300

    add utility types for thunkdispatch and action

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,26 @@
-import { configureStore, getDefaultMiddleware, Action } from "@reduxjs/toolkit";
+import {
+  configureStore,
+  getDefaultMiddleware,
+  Action,
+  ThunkAction as ReduxThunkAction,
+  ThunkDispatch as ReduxThunkDispatch,
+} from "@reduxjs/toolkit";
 import { createWrapper } from "next-redux-wrapper";
 
 import reducer, { State } from "./reducer";
 
 export const middleware = [...getDefaultMiddleware({})];
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type ThunkAction<T = void, U = any> = ReduxThunkAction<
+  T,
+  State,
+  unknown,
+  Action<U>
+>;
+export type ThunkDispatch<
+  T extends ThunkAction = ThunkAction
+> = ReduxThunkDispatch<State, unknown, Action<T>>;
 
 export const getStore = () => {
   const store = configureStore<State, Action<unknown>, typeof middleware>({
