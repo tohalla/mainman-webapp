ad84784d5c1958f9fc9a5c2b07abc2cd3f40e370
Touko Hallasmaa <touko.hallasmaa@upcloud.com>
2020-06-13 12:13:23 +0300

    add generic select element

---
+++ src/general/Select.tsx
@@ -0,0 +1,26 @@
+import React, { DetailedHTMLProps, SelectHTMLAttributes } from "react";
+import { Box, Flex } from "rebass";
+
+interface Props
+  extends DetailedHTMLProps<
+    SelectHTMLAttributes<HTMLSelectElement>,
+    HTMLSelectElement
+  > {
+  label: JSX.Element;
+  name: string;
+}
+
+const Select = ({ children, name, label, ...props }: Props) => (
+  <Flex alignItems="center">
+    <Box as="label" htmlFor={name} mr={4}>
+      {label}
+    </Box>
+    <select name={name} {...props}>
+      {children}
+    </select>
+  </Flex>
+);
+
+Select.displayName = "Select";
+
+export default Select;
