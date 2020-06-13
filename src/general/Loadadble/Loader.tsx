5208a2522ede86947be03fda548c24de19b700fe
Touko Hallasmaa <touko.hallasmaa@upcloud.com>
2020-06-13 16:59:07 +0300

    update loader to respect child size

--- src/general/Loadadble/Loader.tsx
+++ src/general/Loadadble/Loader.tsx
@@ -1,39 +1,44 @@
 import React from "react";
+import { Box } from "rebass";
 
 interface Props {
   fill: string;
   opacity: number;
+  height: number;
 }
 
-const Loader = ({ fill, opacity }: Props) => (
-  <svg
-    fill={fill}
-    height="32"
-    opacity={opacity}
-    viewBox="0 0 32 32"
-    width="32"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <path
-      d="M16 0 A16 16 0 0 0 16 32 A16 16 0 0 0 16 0 M16 4 A12 12 0 0 1 16 28 A12 12 0 0 1 16 4"
-      opacity=".25"
-    />
-    <path d="M16 0 A16 16 0 0 1 32 16 L28 16 A12 12 0 0 0 16 4z">
-      <animateTransform
-        attributeName="transform"
-        dur="0.8s"
-        from="0 16 16"
-        repeatCount="indefinite"
-        to="360 16 16"
-        type="rotate"
+const Loader = ({ fill, opacity, height }: Props) => (
+  <Box height={height} width={height}>
+    <svg
+      fill={fill}
+      height="32"
+      opacity={opacity}
+      viewBox="0 0 32 32"
+      width="32"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M16 0 A16 16 0 0 0 16 32 A16 16 0 0 0 16 0 M16 4 A12 12 0 0 1 16 28 A12 12 0 0 1 16 4"
+        opacity=".25"
       />
-    </path>
-  </svg>
+      <path d="M16 0 A16 16 0 0 1 32 16 L28 16 A12 12 0 0 0 16 4z">
+        <animateTransform
+          attributeName="transform"
+          dur="0.8s"
+          from="0 16 16"
+          repeatCount="indefinite"
+          to="360 16 16"
+          type="rotate"
+        />
+      </path>
+    </svg>
+  </Box>
 );
 
 Loader.defaultProps = {
   fill: "#000000",
   opacity: 0.3,
+  height: 32,
 };
 
 export default Loader;
--- src/general/Loadadble/index.tsx
+++ src/general/Loadadble/index.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode, useRef } from "react";
 
 import styled from "../../theme/styled";
 
@@ -10,23 +10,28 @@ interface Props {
 }
 
 const Loadable = ({ isLoading, children }: Props) => {
+  const el = useRef<HTMLDivElement>(null);
   if (!isLoading) {
-    return <>{children}</>;
+    return <div ref={el}>{children}</div>;
   }
 
   return (
     <Container>
-      <Loader />
+      <Loader height={el.current ? el.current.clientHeight : undefined} />
     </Container>
   );
 };
 
-const Container = styled.div`
+const Container = styled.div<{ height?: number }>`
   display: flex;
   align-self: stretch;
   flex: 1;
   align-items: center;
   justify-content: center;
+
+  svg {
+    ${({ height }) => height && `height: ${height};`}
+  }
 `;
 
 export default Loadable;
