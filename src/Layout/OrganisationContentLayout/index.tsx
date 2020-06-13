1c8ae5a3046b9f2e77f8768ccbec404bd732691f
Touko Hallasmaa <touko.hallasmaa@upcloud.com>
2020-06-13 21:48:10 +0300

    wrap already the base layout with org context provider

--- src/Layout/OrganisationContentLayout/index.tsx
+++ src/Layout/OrganisationContentLayout/index.tsx
@@ -1,8 +1,6 @@
-import React, { ReactFragment, useState } from "react";
+import React, { ReactFragment } from "react";
 
 import DefaultLayout from "..";
-import { Organisation } from "../../organisation";
-import OrganisationContext from "../../organisation/OrganisationContext";
 
 import WithHeading from "./WithHeading";
 
@@ -13,17 +11,9 @@ interface Props {
 const OrganisationContentLayout: (props: Props) => JSX.Element = ({
   children,
 }: Props) => {
-  const [activeOrganisation, setActiveOrganisation] = useState<
-    Organisation | undefined
-  >(undefined);
-
   return (
     <DefaultLayout>
-      <OrganisationContext.Provider
-        value={{ activeOrganisation, setActiveOrganisation }}
-      >
-        <WithHeading>{children}</WithHeading>
-      </OrganisationContext.Provider>
+      <WithHeading>{children}</WithHeading>
     </DefaultLayout>
   );
 };
--- src/Layout/index.tsx
+++ src/Layout/index.tsx
@@ -1,19 +1,29 @@
-import React, { ReactFragment } from "react";
+import React, { ReactFragment, useState } from "react";
 import { Flex } from "rebass";
 
 import MainNavigation from "../general/Navigation/MainNavigation";
+import { Organisation } from "../organisation";
+import OrganisationContext from "../organisation/OrganisationContext";
 
 interface Props {
   children: ReactFragment;
 }
 
-const DefaultLayout: (props: Props) => JSX.Element = ({ children }: Props) => (
-  <>
-    <MainNavigation />
-    <Flex as="main" flex={1} flexDirection="column" mx={[2, 5]} my={[2, 5]}>
-      {children}
-    </Flex>
-  </>
-);
+const DefaultLayout: (props: Props) => JSX.Element = ({ children }: Props) => {
+  const [activeOrganisation, setActiveOrganisation] = useState<
+    Organisation | undefined
+  >(undefined);
+
+  return (
+    <OrganisationContext.Provider
+      value={{ activeOrganisation, setActiveOrganisation }}
+    >
+      <MainNavigation />
+      <Flex as="main" flex={1} flexDirection="column" mx={[2, 5]} my={[2, 5]}>
+        {children}
+      </Flex>
+    </OrganisationContext.Provider>
+  );
+};
 
 export default DefaultLayout;
