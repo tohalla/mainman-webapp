8d96b80de8f4cb030bff6ef3caf832e0175ac254
Touko Hallasmaa <hallasmaa.touko@gmail.com>
2020-07-22 19:51:59 +0300

    upgrade rest of deps

--- server/index.ts
+++ server/index.ts
@@ -29,7 +29,7 @@ const getLocaleDataScript = (locale: string) => {
   const lang = locale.split("-")[0];
   if (!localeDataCache.has(lang)) {
     const localeDataFile = require.resolve(
-      `@formatjs/intl-relativetimeformat/dist/locale-data/${lang}`
+      `@formatjs/intl-relativetimeformat/locale-data/${lang}`
     );
     const localeDataScript = readFileSync(localeDataFile, "utf8");
     localeDataCache.set(lang, localeDataScript);
