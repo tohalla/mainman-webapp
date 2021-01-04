d3ae51c3ec5b0d16dfaa6656feafc472d2a1469e
Touko Hallasmaa <hallasmaa.touko@gmail.com>
2021-01-04 19:10:29 +0200

    fix: useRootNode

--- src/hooks/useRootNode.tsx
+++ src/hooks/useRootNode.tsx
@@ -1,7 +1,10 @@
 import { useMemo } from "react";
 
 const useRootNode = () => {
-  return useMemo(() => document.getElementById("__next"), []);
+  return useMemo(
+    () => typeof document !== "undefined" && document.getElementById("__next"),
+    [typeof document === "undefined"]
+  );
 };
 
 export default useRootNode;
