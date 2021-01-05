ecc1bbcd37159177500dbb059cf974b4be06682f
Touko Hallasmaa <hallasmaa.touko@gmail.com>
2021-01-05 12:50:35 +0200

    fix: should use organisationcontentlayout for the overview page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,16 +2,19 @@ import React from "react";
 import { FormattedMessage } from "react-intl";
 
 import { titles } from "src/general/messages";
-import DefaultLayout from "src/Layout";
+import OrganisationContentLayout from "src/Layout/OrganisationContentLayout";
 
 const IndexPage = () => {
   return <div />;
 };
 
 IndexPage.displayName = "IndexPage";
-IndexPage.Layout = DefaultLayout;
+IndexPage.Layout = OrganisationContentLayout;
 IndexPage.layoutProps = {
   title: <FormattedMessage {...titles.overview} />,
+  options: {
+    organisationSelect: false,
+  },
 };
 
 export default IndexPage;
