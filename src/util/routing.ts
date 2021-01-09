af1bc38808dbc2f04b114bfa8d6841dbf3ed229f
Touko Hallasmaa <hallasmaa.touko@gmail.com>
2021-01-09 20:28:09 +0200

    chore: ctx hsould be optional

--- src/util/routing.ts
+++ src/util/routing.ts
@@ -8,15 +8,15 @@ export const redirect = ({
   location,
   condition,
 }: {
-  ctx: Omit<NextPageContext, "store">;
+  ctx?: Omit<NextPageContext, "store">;
   location: string;
   condition?(route: string): boolean;
 }): void => {
-  const route = ctx.req?.url ?? Router.route;
+  const route = ctx?.req?.url ?? Router.route;
   if (typeof condition === "function" && !condition(route)) {
     return;
   }
-  if (ctx.res) {
+  if (ctx?.res) {
     if (route === location) {
       return;
     }
