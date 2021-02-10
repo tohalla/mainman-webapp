f490fe821940ba14ea212310ba0579127f219d95
Touko Hallasmaa <hallasmaa.touko@gmail.com>
2021-02-10 20:20:33 +0200

    fix: should only select on enter

--- src/general/SelectOnClick.tsx
+++ src/general/SelectOnClick.tsx
@@ -4,23 +4,27 @@ interface Props {
   children: ReactNode;
 }
 
-const SelectOnClick = ({ children }: Props) => {
-  const handleTokenSelect = (event: MouseEvent | KeyboardEvent) => {
-    if (typeof window !== "undefined") {
-      const selection = window.getSelection();
-      const r = document.createRange();
-      r.selectNodeContents(event.target as HTMLSpanElement);
-      if (selection) {
-        selection.removeAllRanges();
-        selection.addRange(r);
-      }
+const handleTokenSelect = (event: MouseEvent | KeyboardEvent) => {
+  if (typeof window !== "undefined") {
+    const selection = window.getSelection();
+    const r = document.createRange();
+    r.selectNodeContents(event.target as HTMLSpanElement);
+    if (selection) {
+      selection.removeAllRanges();
+      selection.addRange(r);
     }
-  };
+  }
+};
 
+const SelectOnClick = ({ children }: Props) => {
   return (
     <span
       onClick={handleTokenSelect}
-      onKeyPress={handleTokenSelect}
+      onKeyPress={(event) => {
+        if (event.key === "Enter") {
+          handleTokenSelect(event);
+        }
+      }}
       role="button"
       tabIndex={0}
     >
