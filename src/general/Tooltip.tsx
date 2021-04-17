593d4296baf21f9c732e9112f46192ac82c936ea
Touko Hallasmaa <hallasmaa.touko@gmail.com>
2021-04-17 18:08:00 +0300

    feat: tooltips

---
+++ src/general/Tooltip.tsx
@@ -0,0 +1,9 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import "tippy.js/dist/tippy.css";
+
+import Tippy, { TippyProps } from "@tippyjs/react";
+import React from "react";
+
+const Tooltip = (props: TippyProps) => <Tippy {...props} />;
+
+export default Tooltip;
