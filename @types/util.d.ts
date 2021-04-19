d9a209657f09056af2a7e69b4c4505277f0607be
Touko Hallasmaa <hallasmaa.touko@gmail.com>
2021-04-19 19:35:09 +0300

    chore: utility types for camelizing keys

--- @types/util.d.ts
+++ @types/util.d.ts
@@ -2,3 +2,16 @@ declare type Creatable<T, OmitAdditional extends keyof T = undefined> = Omit<
   T,
   "createdAt" | "updatedAt" | "createdBy" | "id" | "uuid" | OmitAdditional
 >;
+
+type CamelizeString<T extends PropertyKey> = T extends string
+  ? string extends T
+    ? string
+    : T extends `${infer F}_${infer R}`
+    ? `${F}${Capitalize<CamelizeString<R>>}`
+    : T
+  : T;
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+declare type Camelize<T> = T extends object
+  ? { [K in keyof T as CamelizeString<K>]: Camelize<T[K]> }
+  : T;
