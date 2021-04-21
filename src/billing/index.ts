58ce971aa228396bc2a143b8ae756d2bed6928a9
Touko Hallasmaa <hallasmaa.touko@gmail.com>
2021-04-21 22:07:36 +0300

    chore: type fixes

--- src/billing/index.ts
+++ src/billing/index.ts
@@ -1,17 +1,22 @@
-import { PaymentMethod } from "@stripe/stripe-js";
+import { PaymentMethod as StripePaymentMethod } from "@stripe/stripe-js";
 
 import { getApiCall } from "src/util/api";
 
 export const fetchPaymentMethods = () =>
-  getApiCall<Camelize<PaymentMethod>, Record<string, Camelize<PaymentMethod>>>(
+  getApiCall<PaymentMethod, Record<string, PaymentMethod>>(
     `/billing/stripe/payment-methods`
   )({
     responseType: "json",
     key: "id",
   });
 
-export const createPaymentMethod = (payload: PaymentMethod) =>
-  getApiCall<Camelize<PaymentMethod>>("/billing/stripe/payment-methods", {
+export const createPaymentMethod = (payload: StripePaymentMethod) =>
+  getApiCall<PaymentMethod>("/billing/stripe/payment-methods", {
     method: "POST",
     body: payload,
   })({ key: "id", responseType: "json" });
+export type PaymentMethod = Camelize<StripePaymentMethod>;
+
+export type CardPaymentMethod = PaymentMethod & {
+  card: StripePaymentMethod.Card;
+};
